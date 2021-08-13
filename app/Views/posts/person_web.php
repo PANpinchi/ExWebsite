ae1f354e05d92aa15fd3a393284fca05fbda0ac1
Percy5055 <88412230+Percy5055@users.noreply.github.com>
2021-08-13 20:15:02 +0800

    半成品

---
+++ app/Views/posts/person_web.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/person_web.css">
+ 
+  </head>
+
+  <body>
+
+            <table class="c">
+                <tr>
+                    <td rowspan="3"> <img src="/person_img/form_top_01.jpg" >
+                    <td clospan="4"> <img src="/person_img/form_top_02.jpg" >
+                <tr>
+                    <td><img src="/person_img/form_top_03.jpg"  >
+                    <td><img src="/person_img/form_top_04_home_up.jpg"  >
+                    <td><img src="/person_img/form_top_05_star_up.jpg"  >
+                    <td><img src="/person_img/form_top_06.jpg"  >
+                <tr>
+                    <td> <img src="/person_img/form_top_07.jpg"  >
+                <tr>
+                    <td><img src="/person_img/upbar_01.jpg" >
+                    <td><img src="/person_img/upbar_02.jpg" >
+                    <td><img src="/person_img/upbar_03.jpg" >
+                    <td><img src="/person_img/upbar_04.jpg" >
+                    <td><img src="/person_img/upbar_05.jpg" >
+                    <td><img src="/person_img/upbar_06.jpg" >
+                    <td><img src="/person_img/upbar_07.jpg" >
+                    <td><img src="/person_img/upbar_08.jpg" >
+                    <td><img src="/person_img/upbar_09.jpg" >
+                    <td><img src="/person_img/upbar_10.jpg" >
+                <tr>
+                    <td><img src="/person_img/personal_pass_bar.jpg" >
+                    <td><img src="/person_img/subtitle01.jpg" >
+                <tr>
+                    <td><img src="/person_img/covid_19_response.jpg" >
+                    <td><img src="/person_img/icon_16.jpg" >
+                <tr>
+                    <td><img src="/person_img/main_menu_title.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_01.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_02.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_03.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_04.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_05.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_10.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_06.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_07.jpg" >
+                <tr>
+                   <td><img src="/person_img/main_menu_08.jpg" >
+                <tr>
+                    <td><img src="/person_img/main_menu_09.jpg" >
+                <tr>
+                    <td><img src="/person_img/main_menu_fun_col.jpg" >
+                <tr>
+                    <td><img src="/person_img/main_menu_fun_hs.jpg" >
+            </table>
+           
+            
+            
+
+
+
+  
+    
+      
+
+    
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
