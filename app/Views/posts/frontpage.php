68df161cabf076f28a118476531e2222009ad91c
Percy5055 <88412230+Percy5055@users.noreply.github.com>
2021-08-17 13:52:36 +0800

    前端首頁完成(連結位置須改)

--- app/Views/posts/frontpage.php
+++ app/Views/posts/frontpage.php
@@ -11,27 +11,29 @@
     </head>
 
     <body>
-                <div id="portal_page_box">
-                     <div class="texttop">大學招生委員會聯合會</div>
-                     <div id="line"></div>
-                     <div id="logo">
-                            <div class="row">
-                                <div id="pic"></div>
-                                <div id="balloon"></div>
-                            </div>
-                     </div>
-                <br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br>
-                <br><br><br><br><br><br><br><br>
+        <div id="portal_page_box">
+            <div class="texttop">大學招生委員會聯合會</div>
+            <div id="line"></div>
+            <div id="logo">
+                <div class="row">
+                    <div id="pic">
+                        <a href="/PostController/login"><div id = "star"></div></a>  
+                        <a href="/PostController/person_web"> <div id = "person"> </div></a>  
+                     </div> 
+                    <div id="balloon"></div>
+                 </div>
+             </div>
             
+            <br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br>
 
-                     <div class="textdown">建議瀏覽環境：Chrome、FireFox、IE10 以上</div>
-                     <div id="line"></div>
-                     <div class="textdown">
-                         62102嘉義縣民雄鄉大學路一段168號<br>
-                        Copyright by CAC. All rights reserved.   隱私權聲明
-                      </div>
-                </div> 
-        
+            <div class="textdown">建議瀏覽環境：Chrome、FireFox、IE10 以上</div>
+            <div id="line"></div>
+            <div class="textdown">
+                62102嘉義縣民雄鄉大學路一段168號<br>
+                Copyright by CAC. All rights reserved.   隱私權聲明
+             </div>
+         </div> 
     <body>
 </html>
